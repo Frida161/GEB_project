e3e38286d32cac7f64b11386507aaea013562df3
ZhiQing-R <119010253@link.cuhk.edu.cn>
2022-04-07 19:41:20 +0800

    Update simulator

---
+++ Assets/Scripts/Bus.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bus : MonoBehaviour
+{
+
+    public Vector2 location;
+    public Vector2 forward;
+    public Vector2 target;
+    public int ID;
+    public int line;
+    public int dir;
+    public float speed = 10f;
+    public int status = -1;
+    public float waiting = 0;
+
+
+    private void FixedUpdate()
+    {
+        if (status == -1) return;
+        if (waiting > 0)
+        {
+            waiting -= Time.deltaTime;
+            return;
+        }
+
+        //Check if at any bus stops
+        if ((location-target).magnitude < 0.00001f)
+        {
+            location = BusSimulator.Lines[line][status];
+            status += dir;
+            if (status == 0 || status == 9)
+            {
+                Arrived();
+            }
+            else if(BusSimulator.Stops.Contains(status))
+            {
+                waiting = 1f;
+                target = BusSimulator.Lines[line][status + dir];
+                forward = (target - location).normalized;
+                Debug.Log("Arrive at " + status.ToString());
+            }
+            
+        }
+        else
+        {
+            location += forward * speed * Time.deltaTime;
+            //Debug.Log("Current postion: " + location.ToString());
+            Debug.Log(speed * Time.deltaTime);
+        }
+        
+    }
+
+    public void Initialize(Vector2 loc, int id, int l, int d)
+    {
+        location = loc;
+        ID = id;
+        line = l;
+        dir = d;
+    }
+
+    public void Arrived()
+    {
+        if (status == 0)
+        {
+            Debug.Log("Arrive at parking lot upper campus");
+            BusSimulator.singleton.ParkingLotUpper.Enqueue(this);
+            dir = 1;
+        }
+        else
+        {
+            dir = -1;
+            if (line == 0)
+            {
+                Debug.Log("Arrive at parking lot libary");
+                BusSimulator.singleton.ParkingLotLib.Enqueue(this);
+            }
+            else
+            {
+                Debug.Log("Arrive at parking lot TB");
+                BusSimulator.singleton.ParkingLotTB.Enqueue(this);
+            }
+        }
+        status = -1;
+    }
+
+    public void SetBus(int l)
+    {
+        line = l;
+        if (dir == 1)
+        {
+            status = 0;
+            target = BusSimulator.Lines[l][1];
+            forward = (target - location).normalized;
+        }
+        else
+        {
+            status = 9;
+            target = BusSimulator.Lines[l][8];
+            forward = (target - location).normalized;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/BusSimulator.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BusInfo
+{
+    int busId;
+    int dir; //-1: Go upper; 1: Go lower
+    int line; //0 or 1
+    Vector2 location;
+
+    public BusInfo(Bus bus)
+    {
+        busId = bus.ID;
+        dir = bus.dir;
+        line = bus.line;
+        location = bus.location;
+    }
+}
+
+
+public class BusSimulator : MonoBehaviour
+{
+    public static List<Vector2> Line1 = new List<Vector2> 
+    {
+        new Vector2(114.207266f, 22.696781f), //教职工宿舍
+        new Vector2(114.208618f, 22.697022f), //教职-书院拐点
+        new Vector2(114.209234f, 22.696310f), //书院
+        new Vector2(114.209261f, 22.692097f), //中园拐点
+        new Vector2(114.216622f, 22.691858f), //体育馆
+        new Vector2(114.217826f, 22.692925f), //启动区
+        new Vector2(114.218024f, 22.693034f), //1,2号线拐点
+        new Vector2(114.217341f, 22.693813f), //拐点(IF)
+        new Vector2(114.218131f, 22.695234f), //学活
+        new Vector2(114.219380f, 22.6965760f), //图书馆
+    };
+
+    public static List<Vector2> Line2 = new List<Vector2>
+    {
+        new Vector2(114.207266f, 22.696781f), //教职工宿舍
+        new Vector2(114.208618f, 22.697022f), //教职-书院拐点
+        new Vector2(114.209234f, 22.696310f), //书院
+        new Vector2(114.209261f, 22.692097f), //中园拐点
+        new Vector2(114.216622f, 22.691858f), //体育馆
+        new Vector2(114.217826f, 22.692925f), //启动区
+        new Vector2(114.218024f, 22.693034f), //1,2号线拐点
+        new Vector2(114.218455f, 22.692742f), //拐点(TD)
+        new Vector2(114.220224f, 22.694088f), //TD
+        new Vector2(114.221163f, 22.695080f), //TB
+    };
+
+    public static List<Vector2>[] Lines = new List<Vector2>[2] { Line1, Line2 };
+
+    public static Dictionary<string, int> BusStops = new Dictionary<string, int>
+    {
+        {"教职工宿舍", 0},
+        {"书院站", 2},
+        {"体育馆", 4},
+        {"启动区", 5},
+        {"学活", 8},
+        {"图书馆", 9},
+        {"TD", 8},
+        {"TB", 9},
+    };
+
+    public static HashSet<int> Stops = new HashSet<int> { 0, 2, 4, 5, 8, 9};
+    public static BusSimulator singleton;
+
+    public GameObject BusPrefab;
+    public List<Bus> BusList;
+
+    public Queue<Bus> ParkingLotUpper;
+    public Queue<Bus> ParkingLotTB;
+    public Queue<Bus> ParkingLotLib;
+
+    public float time = 0;
+
+    private void Awake()
+    {
+        singleton = this;
+        BusList = new List<Bus>();
+        ParkingLotUpper = new Queue<Bus>();
+        ParkingLotTB = new Queue<Bus>();
+        ParkingLotLib = new Queue<Bus>();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Bus bus = Instantiate(BusPrefab).GetComponent<Bus>();
+            bus.Initialize(Line1[0], i, 0, 1);
+            BusList.Add(bus);
+            ParkingLotUpper.Enqueue(bus);
+        }
+
+        ParkingLotUpper.Dequeue().SetBus(0);
+    }
+
+    public List<BusInfo> GetInfo()
+    {
+        int l = BusList.Count;
+        List<BusInfo> r = new List<BusInfo>();
+        for (int i = 0; i < l; i++)
+        {
+            r.Add(new BusInfo(BusList[i]));
+        }
+        return r;
+    }
+
+
+
+
+}
